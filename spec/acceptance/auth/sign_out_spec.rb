3fd6229452e7a553cc89e19b04c190871631de14
Paul Blaze <paul.ovechkin@gmail.com>
2015-03-25 01:22:12 +0300

    test covered some auth scenarios

--- spec/acceptance/auth/sign_out_spec.rb
+++ spec/acceptance/auth/sign_out_spec.rb
@@ -6,6 +6,15 @@ feature 'User sign out', %q{
   I want to be able to sign out
 } do
 
-  scenario 'Authenticated user signs out'
-  scenario 'Unauthenticated user tries to sign out'
+  given(:user) {create :user}
+
+  scenario 'Authenticated user signs out' do
+    sign_in user
+
+    visit destroy_user_session_path
+    expect(page).to have_content "Signed out successfully."
+
+    visit new_user_registration_path # testing if user is really signed out. If signed in, devise does not allow to sign up
+    expect(page).not_to have_content "You are already signed in."
+  end
 end
--- spec/acceptance/auth/sign_up_spec.rb
+++ spec/acceptance/auth/sign_up_spec.rb
@@ -6,8 +6,33 @@ feature 'User sign up', %q{
   I want to be able to sign up
 } do
 
-  scenario 'Authenticated user tries to sign up'
-  scenario 'Unregistered user tries to sign up with existing identity'
-  scenario 'Unregistered user signs up'
+  given(:user) {create :user}
+
+  scenario 'Authenticated user tries to sign up' do
+    sign_in user
+
+    visit new_user_registration_path
+    expect(page).to have_content "You are already signed in."
+  end
+
+  scenario 'Unregistered user tries to sign up with existing identity' do
+    visit new_user_registration_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_on 'Sign up'
+    expect(page).to have_content "Email has already been taken"
+  end
+
+  scenario 'Unregistered user signs up' do
+    visit new_user_registration_path
+
+    fill_in 'Email', with: 'newuser@example.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+    expect(page).to have_content "Welcome! You have signed up successfully."
+  end
 
 end
