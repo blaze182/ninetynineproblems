506db941ba10f45ba3145424cab5234e39e56b8f
Paul Blaze <paul.ovechkin@gmail.com>
2015-03-28 23:45:54 +0300

    added view questions and answers spec

--- spec/acceptance/qa/view_question_spec.rb
+++ spec/acceptance/qa/view_question_spec.rb
@@ -2,10 +2,25 @@ require 'rails_helper'
 
 feature 'View question with answers', %q{
   In order to find answers to a question
-  As a guest or an authenticated user
+  As a any user
   I want to be able to view question with it's answers
 } do
 
-  scenario 'Authenticated user views question with answers'
-  scenario 'Unauthenticated user views question with answers'
+  given(:user) {create :user}
+  given!(:question_list) {create_list :question, 5}
+  given!(:answers_list) {create_list :answer, 5, question: question_list[0]}
+
+  scenario 'Any user views question with answers' do
+    visit questions_path
+    question_list.each do |question|
+      expect(page).to have_content question.title
+    end
+
+    visit question_path(question_list[0])
+    expect(page).to have_content question_list[0].title
+    expect(page).to have_content question_list[0].body
+    answers_list.each do |answer|
+      expect(page).to have_content answer.body
+    end
+  end
 end
