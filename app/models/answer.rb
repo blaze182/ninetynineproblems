46b5023de162403e175b6cff7e4ab6e08f94e59d
Paul Blaze <paul.ovechkin@gmail.com>
2015-04-24 01:18:55 +0300

    added :best to Answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,11 @@ class Answer < ActiveRecord::Base
   belongs_to :user
 
   validates :body, presence: true, length: { in: 30..30000 }
+
+  def mark_best
+    Answer.transaction do
+      self.question.answers.update_all best: false
+      self.update best: true
+    end
+  end  
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
   it { should validate_length_of(:body).is_at_least(30).is_at_most(30000) }
   it { should belong_to :user }
+
+  describe 'mark answer as the best' do
+    let(:question) { create :question }
+    let(:answer) { create :answer, question: question }
+    let!(:answers) { create_list :answer, 5, question: question, best: true }
+
+    it 'makes answer the best' do
+      answer.mark_best
+      answer.reload
+      expect(answer).to be_best
+    end
+
+    it 'makes the rest of answers not best' do
+      answer.mark_best
+      answers.each do |a|
+        a.reload
+        expect(a).not_to be_best
+      end
+    end
+  end
 end
